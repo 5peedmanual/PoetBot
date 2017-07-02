41099597a7d5d1e69b33fb322662dfd971ccdd55
Chickenmcheese <thewalkingdoge@protonmail.com>
2017-07-02 22:51:40 +0100

    Update tornado.py

--- tornado.py
+++ tornado.py
@@ -7,14 +7,7 @@ import argparse
 
 
 
-"""CONSUMER_KEY = 'M8pKgdbnw9UtXaKgR15CNFWHM'
-CONSUMER_SECRET = 'bhmVEfxUHi0YuU8ivYCGSKRcbKxRv9GKYSbfdMNVMjPnxPiSP5'
-ACCESS_KEY = '842767953917624322-tjRRrRiCYKOuCdRj9rCNkjNzQHddiNB'
-ACCESS_SECRET = 'ltE3tVFIQG7cUtyz5mDGHsTF7FAETe57HIe1ZRe6YGHE4'
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)"""
 
 def choosing_words():
 
